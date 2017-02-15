39e7b86f92feb5b0a64ac338bcbb7a32a1c4fdff
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-02-15 17:20:12 -0500

    listAllMembers() implemented and tested

--- src/main/java/squier/john/typeInformation/Reflector.java
+++ src/main/java/squier/john/typeInformation/Reflector.java
@@ -1,5 +1,13 @@
 package squier.john.typeInformation;
 
+import com.sun.tools.internal.jxc.ap.Const;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
+
 /**
  * Created by johnsquier on 2/15/17.
  */
@@ -15,8 +23,6 @@ public class Reflector {
         }
     }
 
-    // classImplementsInterface(String, String) && classImplementsInterface(Object, String)
-    //      call this method.
     public boolean classImplementsInterface(Class<?> theClass, String theInterface) {
         Class<?>[] implementedInterfaces = theClass.getInterfaces();
         Class<?> theInterfaceClass;
@@ -40,5 +46,96 @@ public class Reflector {
 
     public boolean classImplementsInterface(Object o, String theInterface) {
         return classImplementsInterface(o.getClass(), theInterface);
+    }
+
+    public String listAllMembers(Object o) {
+        StringBuilder sb = new StringBuilder();
+
+        Class<?> theClass = o.getClass();
+
+        sb.append(generateClassInfoString(theClass));
+
+        while ( !theClass.getSimpleName().equals("Object")) {
+            theClass = theClass.getSuperclass();
+            sb.append(generateClassInfoString(theClass));
+        }
+
+        return sb.toString();
+    }
+
+    private String generateClassInfoString(Class<?> theClass) {
+        Field[] fields = theClass.getFields();
+        Constructor<?>[] constructors = theClass.getConstructors();
+        Method[] methods = theClass.getMethods();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Fields\n");
+        for ( Field f : fields ) {
+            sb.append(theClass.getSimpleName()).append(" : ");
+            sb.append(Modifier.toString(f.getModifiers())).append(" ");
+            sb.append(f.getType().getSimpleName()).append(" ");
+            sb.append(f.getName()).append("\n");
+        }
+
+        sb.append("Constructors\n");
+        for ( Constructor<?> c : constructors ) {
+            sb.append(theClass.getSimpleName()).append(" : ");
+            sb.append(Modifier.toString(c.getModifiers())).append(" ");
+            sb.append(c.getName()).append("(");
+
+            Class<?>[] params = c.getParameterTypes();
+            if ( params.length == 0 ) {
+                sb.append(")");
+            }
+            else {
+                for ( int i = 0; i < params.length; i++ ) {
+                    sb.append(params[i].getName());
+                    if (i < params.length - 1) {
+                        sb.append(", ");
+                    } else {
+                        sb.append(")");
+                    }
+                }
+            }
+
+            sb.append("\n");
+        }
+
+        Arrays.sort(methods, new Comparator<Method>() {
+            @Override
+            public int compare(Method o1, Method o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        sb.append("Methods\n");
+        for ( Method m : methods )
+            if ( m.getDeclaringClass().getSimpleName().equals(theClass.getSimpleName()) ) {
+                sb.append(theClass.getSimpleName()).append(" : ");
+                sb.append(Modifier.toString(m.getModifiers())).append(" ");
+                sb.append(m.getReturnType()).append(" ");
+                sb.append(m.getName()).append("(");
+
+                Class<?>[] params = m.getParameterTypes();
+                if ( params.length == 0 ) {
+                    sb.append(")");
+                }
+                else {
+                    for ( int i = 0; i < params.length; i++ ) {
+                        sb.append(params[i].getName());
+                        if (i < params.length - 1) {
+                            sb.append(", ");
+                        } else {
+                            sb.append(")");
+                        }
+                    }
+                }
+
+                sb.append("\n");
+            }
+
+
+        return sb.toString();
     }
 }
--- src/test/java/squier/john/typeInformation/ReflectorTest.java
+++ src/test/java/squier/john/typeInformation/ReflectorTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 /**
@@ -82,10 +84,105 @@ public class ReflectorTest {
     }
 
     @Test
-    public void tryToCompareObjectToNonExistantInterface() {
+    public void tryToCompareObjectToNonExistentInterface() {
         boolean expected = false;
 
-        boolean actual = reflector.classImplementsInterface(new Object(), "java.lang.HocusPocus");
+        boolean actual = reflector.classImplementsInterface(new Object(), "abra.kadabra.Allakhazham");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void listAllMembersBufferedWriter() {
+        String expected = "Fields\n" +
+                "Constructors\n" +
+                "BufferedWriter : public java.io.BufferedWriter(java.io.Writer)\n" +
+                "BufferedWriter : public java.io.BufferedWriter(java.io.Writer, int)\n" +
+                "Methods\n" +
+                "BufferedWriter : public void close()\n" +
+                "BufferedWriter : public void flush()\n" +
+                "BufferedWriter : public void newLine()\n" +
+                "BufferedWriter : public void write([C, int, int)\n" +
+                "BufferedWriter : public void write(int)\n" +
+                "BufferedWriter : public void write(java.lang.String, int, int)\n" +
+                "Fields\n" +
+                "Constructors\n" +
+                "Methods\n" +
+                "Writer : public class java.io.Writer append(java.lang.CharSequence, int, int)\n" +
+                "Writer : public class java.io.Writer append(char)\n" +
+                "Writer : public class java.io.Writer append(java.lang.CharSequence)\n" +
+                "Writer : public volatile interface java.lang.Appendable append(char)\n" +
+                "Writer : public volatile interface java.lang.Appendable append(java.lang.CharSequence, int, int)\n" +
+                "Writer : public volatile interface java.lang.Appendable append(java.lang.CharSequence)\n" +
+                "Writer : public abstract void close()\n" +
+                "Writer : public abstract void flush()\n" +
+                "Writer : public void write([C)\n" +
+                "Writer : public abstract void write([C, int, int)\n" +
+                "Writer : public void write(int)\n" +
+                "Writer : public void write(java.lang.String, int, int)\n" +
+                "Writer : public void write(java.lang.String)\n" +
+                "Fields\n" +
+                "Constructors\n" +
+                "Object : public java.lang.Object()\n" +
+                "Methods\n" +
+                "Object : public boolean equals(java.lang.Object)\n" +
+                "Object : public final native class java.lang.Class getClass()\n" +
+                "Object : public native int hashCode()\n" +
+                "Object : public final native void notify()\n" +
+                "Object : public final native void notifyAll()\n" +
+                "Object : public class java.lang.String toString()\n" +
+                "Object : public final void wait(long, int)\n" +
+                "Object : public final native void wait(long)\n" +
+                "Object : public final void wait()\n";
+
+        String actual = null;
+        try { actual = reflector.listAllMembers(new BufferedWriter(new FileWriter(" "))); }
+        catch ( Exception e ) { }
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void listAllMembersBoolean() {
+        String expected = "Fields\n" +
+                "Boolean : public static final Boolean TRUE\n" +
+                "Boolean : public static final Boolean FALSE\n" +
+                "Boolean : public static final Class TYPE\n" +
+                "Constructors\n" +
+                "Boolean : public java.lang.Boolean(boolean)\n" +
+                "Boolean : public java.lang.Boolean(java.lang.String)\n" +
+                "Methods\n" +
+                "Boolean : public boolean booleanValue()\n" +
+                "Boolean : public static int compare(boolean, boolean)\n" +
+                "Boolean : public int compareTo(java.lang.Boolean)\n" +
+                "Boolean : public volatile int compareTo(java.lang.Object)\n" +
+                "Boolean : public boolean equals(java.lang.Object)\n" +
+                "Boolean : public static boolean getBoolean(java.lang.String)\n" +
+                "Boolean : public static int hashCode(boolean)\n" +
+                "Boolean : public int hashCode()\n" +
+                "Boolean : public static boolean logicalAnd(boolean, boolean)\n" +
+                "Boolean : public static boolean logicalOr(boolean, boolean)\n" +
+                "Boolean : public static boolean logicalXor(boolean, boolean)\n" +
+                "Boolean : public static boolean parseBoolean(java.lang.String)\n" +
+                "Boolean : public class java.lang.String toString()\n" +
+                "Boolean : public static class java.lang.String toString(boolean)\n" +
+                "Boolean : public static class java.lang.Boolean valueOf(java.lang.String)\n" +
+                "Boolean : public static class java.lang.Boolean valueOf(boolean)\n" +
+                "Fields\n" +
+                "Constructors\n" +
+                "Object : public java.lang.Object()\n" +
+                "Methods\n" +
+                "Object : public boolean equals(java.lang.Object)\n" +
+                "Object : public final native class java.lang.Class getClass()\n" +
+                "Object : public native int hashCode()\n" +
+                "Object : public final native void notify()\n" +
+                "Object : public final native void notifyAll()\n" +
+                "Object : public class java.lang.String toString()\n" +
+                "Object : public final void wait(long, int)\n" +
+                "Object : public final native void wait(long)\n" +
+                "Object : public final void wait()\n";
+
+        String actual = reflector.listAllMembers(new Boolean(true));
 
         Assert.assertEquals(expected, actual);
     }
